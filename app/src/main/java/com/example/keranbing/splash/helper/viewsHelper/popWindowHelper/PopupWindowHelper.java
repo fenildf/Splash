6b78c3662ebdbe58419030d7579b13fbd704ef0d
keranbin <kbinran@gmail.com>
2016-08-08 18:01:54 +0800

    firstcommit

--- app/src/main/java/com/example/keranbing/splash/helper/viewsHelper/popWindowHelper/PopupWindowHelper.java
+++ app/src/main/java/com/example/keranbing/splash/helper/viewsHelper/popWindowHelper/PopupWindowHelper.java
@@ -18,34 +18,35 @@ import com.example.keranbing.splash.helper.viewsHelper.numberProgressBarHelper.v
 /**
  * Created by keranbin on 2016/8/7.
  */
-public class PopWindowHelper {
+public class PopupWindowHelper {
     public static NumberProgressBar numberProgressBar;
     public static TextView tvPercentage;
+    public static PopupWindow popupWindow;
 
 
     public static void createDownLoadProgressPopwindow(Activity context, View parent, final Window window, int layout, int tvPercentageId) {
         final View view = LayoutInflater.from(context).inflate(layout, null);
-        numberProgressBar= (NumberProgressBar) view.findViewById(R.id.progressBar);
-        tvPercentage= (TextView) view.findViewById(R.id.tvPercentage);
+        numberProgressBar = (NumberProgressBar) view.findViewById(R.id.progressBar);
+        tvPercentage = (TextView) view.findViewById(R.id.tvPercentage);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         view.setLayoutParams(params);
-        final PopupWindow popupWindow = new PopupWindow(view, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
-        //设置MyPopupWindow的View
+        popupWindow = new PopupWindow(view, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        //设置popupwindow的View
         popupWindow.setContentView(view);
-        //设置MyPopupWindow的View弹出窗体的宽
+        //设置popupwindow的View弹出窗体的宽
         popupWindow.setWidth(ViewGroup.LayoutParams.FILL_PARENT);
-        //设置MyPopupWindow的View弹出窗体的高
+        //设置popupwindow的View弹出窗体的高
         popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
-        //设置MyPopupWindow的View弹出窗体可点击,如果不添加这个属性，那么点击EditText无法弹出输入法
-        popupWindow.setFocusable(true);
-        //设置MyPopupWindow去除边际黑线
+        //设置点击popupwindow外部区域不隐藏popupwindow
+        popupWindow.setFocusable(false);
+        popupWindow.setOutsideTouchable(false);
+        //设置popupwindow去除边际黑线
         popupWindow.setBackgroundDrawable(new BitmapDrawable());
         //避免输入法覆盖掉popWindow
         popupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         popupWindow.showAtLocation(parent, Gravity.CENTER, 0, 0);
 
         setWindowGray(popupWindow, window);
-
 
     }
 
@@ -61,12 +62,29 @@ public class PopWindowHelper {
         }
     }
 
+    /*
+    * @params progressHelperListener
+    * 回调进度条和进度数字给Activity
+    * */
     public static void setProgress(ProgressHelperListener progressHelperListener) {
-      progressHelperListener.setProgress(numberProgressBar,tvPercentage);
+        progressHelperListener.setProgress(numberProgressBar, tvPercentage);
     }
 
-    public interface ProgressHelperListener{
-        public void setProgress(NumberProgressBar numberProgressBar, TextView tvPercentage);
+    /*
+    * @params progressHelperDismissListener
+    * 回调popupwindow给Activity，下载完成的时候关闭popupwindow
+    * */
+    public static void dismissProgress(ProgressHelperDismissListener progressHelperDismissListener) {
+        progressHelperDismissListener.dismissProgress(popupWindow);
+    }
+
+
+    public interface ProgressHelperListener {
+        void setProgress(NumberProgressBar numberProgressBar, TextView tvPercentage);
+    }
+
+    public interface ProgressHelperDismissListener {
+        void dismissProgress(PopupWindow popupWindow);
     }
 
 }
